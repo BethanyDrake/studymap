1ff510f1d2709f08a53207b22da01f0dfb2513bc
Bethany Drake <bethanyd@thoughtworks.com>
2018-05-02 13:37:16 +1000

    buttons have simple onclick functions

--- popup.js
+++ popup.js
@@ -1,1 +1,5 @@
 console.log("hi there!");
+const saveButton = document.getElementById("save");
+const viewButton = document.getElementById("view");
+saveButton.onclick = function(){console.log("saved!")};
+viewButton.onclick = function(){console.log("viewed!")};
