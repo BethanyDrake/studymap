eff198d3ac0d28376e39176353757be749ec93bd
Bethany Drake <bethanyd@thoughtworks.com>
2018-05-04 09:42:21 +1000

    words are displayed as list

--- view-wordlist.js
+++ view-wordlist.js
@@ -1,6 +1,27 @@
+
+const wordlist = document.getElementById("wordlist");
+const toListItem = function (string)
+{
+   var listItem = document.createElement("li");
+   listItem.innerHTML = string;
+   return listItem;
+};
+
+const toList = function (list, nextListItem)
+{
+  list.appendChild(nextListItem);
+  return list;
+};
+
+const stringToHtmlList = function (string){
+  return string.split(',').map(toListItem)
+                  .reduce(toList, wordlist);
+};
+
 const displayWordlist = function(savedData)
 {
-  document.getElementById("wordlist").textContent = savedData.wordlist;
+  wordlist.innerText = '';
+  stringToHtmlList(savedData.wordlist);
 
 };
 chrome.storage.sync.get('wordlist', displayWordlist);
