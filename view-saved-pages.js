696a2e86e90a7fe968b67b3dba23b4d610540ad5
Bethany Drake <bethanyd@thoughtworks.com>
2018-05-03 12:39:44 +1000

    displays only pages that contain a hardcoded query

--- view-saved-pages.js
+++ view-saved-pages.js
@@ -30,5 +30,66 @@ const clearBookmarks = function(){
   savedPagesNode.innerHTML = '';
 
 };
-
 document.getElementById('clear').onclick=clearBookmarks;
+
+
+
+
+
+
+//view page text
+function loadXMLDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerText = this.responseText;
+      var responseString = this.responseText;
+      /*
+      var responseXML = this.responseXML;
+      parse response string
+      parser = new DOMParser()
+      doc = parser.parseFromString(responseString, "text/html");
+      console.log(doc.getElementsByTagName('body'));
+      */
+      console.log(responseString);
+    }
+  };
+  xhttp.open("GET", "http://www.unicorn.com.au/", true);
+  xhttp.send();
+};
+
+const searchQuery = 'unicorn';
+
+const containsSearchQuery = function(pageURL){
+  var xhttp = new XMLHttpRequest();
+  var done = 'no response recieved';
+  var result = false;
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var responseString = this.responseText;
+
+      result = responseString.includes(searchQuery);
+    }
+
+  };
+  xhttp.open("GET", pageURL, false);
+  xhttp.send();
+  console.log(done);
+
+  console.log(pageURL + ": result: " + result);
+  return result;
+};
+
+const displayRelevantPages =  function(result) {
+          var splitPages = result.pages.split(',');
+          savedPagesNode.innerHTML = '';
+          splitPages.filter(containsSearchQuery)
+                    .map(toLinkNode)
+                    .reduce(toSingleNode, savedPagesNode);
+        };
+
+chrome.storage.sync.get('pages', displayRelevantPages);
+
+
+
+//loadXMLDoc();
