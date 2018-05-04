baf450147789105f72ae4f5b642ada7eb0f6dfb9
Bethany Drake <bethanyd@thoughtworks.com>
2018-05-04 16:02:02 +1000

    no duplicate words

--- addword.js
+++ addword.js
@@ -2,13 +2,29 @@ console.log("addwordscript is running!");
 var wordToAdd = window.getSelection().toString();
 console.log("word to add: " + wordToAdd);
 
+
+var notEqualTo = function(thing){
+  return function (word){
+    if (word == thing) return false;
+    return true;
+  }
+};
+
+
+var concatWithCommas = function(accumulator, currentValue){
+  return accumulator + ',' + currentValue;
+};
+
 var saveWordToAdd = function(savedData)
 {
   if (!savedData.wordlist)
   {
     chrome.storage.sync.set({'wordlist':''});
   }
-    var newWordlist = wordToAdd +"," +savedData.wordlist;
+
+    var oldList = savedData.wordlist.split(',');
+    var filteredOldList = oldList.filter(notEqualTo(wordToAdd)).reduce(concatWithCommas, '');
+    var newWordlist = wordToAdd +"," +filteredOldList;
     console.log("saving... " + wordToAdd);
     chrome.storage.sync.set({'wordlist':newWordlist});
 
